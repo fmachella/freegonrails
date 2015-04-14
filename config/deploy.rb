aa4043c65339d0cd90b3594bf9a9ee3a817a6e5b
Fabrizio <fabrizio.machella@gmail.com>
2015-04-14 21:51:37 +0200

    cambio execjs engine per bug più deploy con variabile rails

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,8 @@ set :scm, :git
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :rails_env, :production
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
